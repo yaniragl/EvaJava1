fa6eb49819e93600b9b16f09256fd9a9fee80fc2
yaniragl <yanira_galeas@hotmail.com>
2017-10-31 19:19:51 -0300

    Inicializacion del repositorio y creacion de clases

---
+++ src/evajava1/Rectangulo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evajava1;
+
+/**
+ *
+ * @author ceduc
+ */
+public class Rectangulo {
+    
+}
